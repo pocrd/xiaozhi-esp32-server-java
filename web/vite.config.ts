7731a3c831c575cbdfeaa480370f3123ce678a40
pocrd <pocrd@poc>
2026-03-17 13:54:49 +0800

    [dev] update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 8084,
       host: '0.0.0.0',
+      allowedHosts: ['xz.caringfamily.cn'],
       proxy: {
         '/api': {
           target: process.env.API_URL || env.VITE_BACKEND_URL || 'http://localhost:8091',
